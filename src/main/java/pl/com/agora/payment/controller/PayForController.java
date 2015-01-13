e99f54eb18668575f338a766cc05109742dad1fc
Piotr Prusiński <pprusinski@agora.pl>
2015-01-13 15:03:54 +0100

    stash

--- src/main/java/pl/com/agora/payment/controller/PayForController.java
+++ src/main/java/pl/com/agora/payment/controller/PayForController.java
@@ -1,5 +1,7 @@
 package pl.com.agora.payment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PayForController {
 
+	@Autowired
+	private CounterService counterService;
+	
 	@RequestMapping("/payfor")
 	@ResponseBody
 	public String payFor(@RequestParam String id) {
