259801c68c65af0e0d435954d7f73020270b9907
Piotr Prusiński <pprusinski@agora.pl>
2015-01-15 14:12:47 +0100

    own metrics

--- src/main/java/pl/com/agora/payment/controller/PayForController.java
+++ src/main/java/pl/com/agora/payment/controller/PayForController.java
@@ -1,21 +1,25 @@
 package pl.com.agora.payment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+
 @RestController
 public class PayForController {
 
 	@Autowired
-	private CounterService counterService;
-	
+	MetricRegistry metrics;
+
 	@RequestMapping("/payfor")
 	@ResponseBody
 	public String payFor(@RequestParam String id) {
+		Meter samplerequest = metrics.meter("samplerequest");
+		samplerequest.mark();
 		return "ok";
 	}
 }
