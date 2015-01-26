ccb4c9adda4ac1737f1cd3dd0ea6fbd68f05e7b3
Mikolaj Gierulski <mikolaj.gierulski@agora.pl>
2015-01-26 16:12:50 +0100

    zmiana zwracanego wyniku na klase PayForResult (zeby sie json zwracal)

--- src/main/java/pl/com/agora/payment/controller/PayForController.java
+++ src/main/java/pl/com/agora/payment/controller/PayForController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.com.agora.payment.dto.PayForResult;
+
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 
@@ -17,9 +19,11 @@ public class PayForController {
 
 	@RequestMapping("/payfor")
 	@ResponseBody
-	public String payFor(@RequestParam String id) {
+	public PayForResult payFor(@RequestParam String id) {
 		Meter samplerequest = metrics.meter("samplerequest");
+		PayForResult payForResult = new PayForResult();
+		payForResult.setResult("ok");
 		samplerequest.mark();
-		return "ok";
+		return payForResult;		
 	}
 }
---
+++ src/main/java/pl/com/agora/payment/dto/PayForResult.java
@@ -0,0 +1,16 @@
+package pl.com.agora.payment.dto;
+
+public class PayForResult {
+
+	private String result;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+}
